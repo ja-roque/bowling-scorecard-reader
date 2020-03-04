abbf3f89b5f76a4ccc2e00abce8c361f113878a9
Javier Roque <jarb95@gmail.com>
2020-03-04 13:24:20 -0600

    Added scorecard bowling spec

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,9 +1,29 @@
 require 'rspec'
+require_relative '../scorecard/score_card'
 
 describe 'Bowling' do
 
-  it 'Should read text scorecard file' do
-    true.should == false
+  let(:model_score_hash) do
+    {
+        'Jeff' => %w[10 7 3 9 0 10 0 8 8 2 F 6 10 10 10 8 1],
+        'John' => %w[3 7 6 3 10 8 1 10 10 9 0 7 3 4 4 10 9 0]
+    }
+  end
+
+  it 'Should read the file' do
+
+    scorecard = ScoreCard::TextFile.new('sample.txt')
+    scorecard_file = scorecard.read
+    expect(scorecard_file).to be_a(String)
+
+  end
+
+  it 'Should parse text file to hash' do
+    scorecard = ScoreCard::TextFile.new('sample.txt')
+    scorecard.read
+    file_hash = scorecard.parse
+    expect(file_hash).to eq(model_score_hash)
+
   end
 
 end
