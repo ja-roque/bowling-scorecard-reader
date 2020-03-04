8626b7c3a8ccca1f7a108c86eb8535352d1cfac6
Javier Roque <jarb95@gmail.com>
2020-03-04 11:34:59 -0600

    Created script initializer and bowling class development.

--- bowling.rb
+++ bowling.rb
@@ -1,2 +1,17 @@
+require_relative 'scorecard/score_card'
+
 class Bowling
+  attr_accessor :file
+
+  def initialize(scorecard_file)
+    puts '* Reading the scorecard file *'
+    @file = File.read(scorecard_file)
+    start
+  end
+
+  def start
+    player_scores_hash = ScoreCard::TextFile.new(file).parse
+    puts player_scores_hash
+  end
+
 end
---
+++ start_bowling.rb
@@ -0,0 +1,3 @@
+require_relative 'bowling'
+
+Bowling.new ARGV[0]
