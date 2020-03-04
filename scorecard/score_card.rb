9753baa9b9c3bef21f1fcaa9343c1d05f0ce0750
Javier Roque <jarb95@gmail.com>
2020-03-04 13:24:41 -0600

    Refactored scorecard

--- scorecard/score_card.rb
+++ scorecard/score_card.rb
@@ -2,14 +2,18 @@ module ScoreCard
 
   class TextFile
 
-    attr_reader :file
+    attr_reader :filename, :scorecard_file
 
-    def initialize(file)
-      @file = file
+    def initialize(filename)
+      @filename = filename
+    end
+
+    def read
+      @scorecard_file = File.read(filename)
     end
 
     def parse
-      player_score_array = file.split("\n")
+      player_score_array = scorecard_file.split("\n")
       score_pairs = player_score_array.map(&:split)
       score_pairs.each_with_object({}) do |pair, hash|
         hash[pair.first] ||= []
