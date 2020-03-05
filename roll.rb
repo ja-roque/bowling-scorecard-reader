78cec6e75324db399bdb6d9a31f8fa92a72b1bf6
Javier Roque <jarb95@gmail.com>
2020-03-04 21:03:14 -0600

    Created valid roll values.

--- roll.rb
+++ roll.rb
@@ -2,21 +2,12 @@ class Roll
 
   attr_accessor :pins, :foul, :display
 
+  VALID_ROLL_VALS = (0..10).to_a + %W['F','f']
+
   def initialize(roll_data)
+    # Raise BowlingError("Invalid Roll Data #{roll_data}") unless VALID_ROLL_VALS.include? roll_data
     @pins = roll_data.to_i
     @display = pins
-  end
-
-  def identify_roll(roll_data)
-    if roll_data.downcase == 'f'
-      foul!
-    elsif pins.positive? && pins <= 10
-      strike!
-    elsif pins.zero?
-      # Can display 0s as ' - '
-    else
-      puts 'Raise invalid roll data'
-    end
   end
 
   def strike!
@@ -30,6 +21,18 @@ class Roll
   def foul!
     @foul = true
     @display = 'F'
+  end
+
+  def identify_roll(roll_data)
+    if roll_data.downcase == 'f'
+      foul!
+    elsif pins.positive? && pins <= 10
+      strike!
+    elsif pins.zero?
+      # Can display 0s as ' - '
+    else
+      puts 'Raise invalid roll data'
+    end
   end
 
   def strike?
