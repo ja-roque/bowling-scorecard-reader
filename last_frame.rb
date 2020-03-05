8bd836ca0891d7838bc70afb1d9ead6ef4b7a1db
Javier Roque <jarb95@gmail.com>
2020-03-04 20:42:12 -0600

    Added special check method for LastFrame
    This is necessary because the last frame has a different criteria to determine if it should keep rolling or not.

--- last_frame.rb
+++ last_frame.rb
@@ -1,4 +1,16 @@
 class LastFrame < Frame
   DEFAULT_ROLL_COUNT = 3
 
+  def check
+    num_rolls = rolls.length
+    @stop_rolling = if num_rolls >= 3
+                      rolls[2].spare! if rolls[-1].pins + rolls[-2].pins == 10
+                      true
+                    elsif num_rolls > 1 && (rolls[0].pins + rolls[1].pins == 10)
+                      rolls[1].spare!
+                    elsif num_rolls > 1 && !rolls[0].strike? && (rolls[0].pins + rolls[1].pins != 10)
+                      true
+                    end
+  end
+
 end
